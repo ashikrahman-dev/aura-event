5de0fdb286b1add707a287980ffe7fe50370e7a1
ashikrahman-dev <ashikur1993@gmail.com>
2024-04-16 19:09:51 +0600

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,59 @@
+//Invention slider
+document.addEventListener("DOMContentLoaded", function () {
+    const eventSlider = document.querySelectorAll("#eventSlider");
+
+    eventSlider.forEach(function (element) {
+        const splide = new Splide(element, {
+            perPage: 3,
+            perMove: 1,
+            gap: "30px",
+            pagination: false,
+            arrows: false,
+            padding: "190px",
+            trimSpace: false,
+            focus: 0.38,
+        });
+
+        document
+            .querySelector(".splide_next")
+            .addEventListener("click", (e) => {
+                splide.go("+1");
+            });
+
+        document
+            .querySelector(".splide_prev")
+            .addEventListener("click", (e) => {
+                splide.go("-1");
+            });
+
+        const bar = splide.root.querySelector(".event-slider-progress-bar");
+        splide.on("mounted move", function () {
+            const count =
+                window.innerWidth > 1200 ? 3 : window.innerWidth > 767 ? 2 : 1;
+            const end = splide.Components.Controller.getEnd() + count;
+            const rate = Math.min((splide.index + 1) / end, 1);
+            bar.style.width = String(100 * rate) + "%";
+        });
+
+        splide.mount();
+    });
+});
+// dev js start
+
 const prevBtns = document.querySelectorAll(".btn-prev");
 const nextBtns = document.querySelectorAll(".btn-next");
 const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
 const progressSteps = document.querySelectorAll(".progress-step");
-// const addExperienceBtn = document.querySelector(".add-exp-btn");
-// const experiencesGroup = document.querySelector(".experiences-group");
-// const btnComplete = document.querySelector(".btn-complete");
-// btnComplete.addEventListener("click", () => {
-//     document.getElementsByTagName('form').submit
-// })
+
 let formStepsNum = 0;
 let experienceNum = 1;
 
-// addExperienceBtn.addEventListener("click", () => {
-//     experienceNum++;
-//     let text = `
-//         <hr>
-//     <div class='experience-item'>
-//         <div class='input-group' >
-//         <label for='title'>Company name</label>
-//         <input type='text' name='title[]' id='title'>
-//     </div>
-//     <div class='input-group'>
-//         <label for='start-date'>Start date</label>
-//         <input type='date' name='start-date[]' id='start-date'>
-//     </div>
-//     <div class='input-group'>
-//         <label for='end-date'>End date</label>
-//         <input type='date' name='nd-date[]' id='end-date'>
-//     </div>
-//     <div class='input-group'>
-//         <label for='job-description'>Description</label>
-//         <textarea name='job-description[]' id='job-description' cols='42' rows='6'></textarea>
-//     </div>
-// </div > `
-//     experiencesGroup.insertAdjacentHTML('beforeend', text);
-// })
-
 function updateFormSteps() {
-
-    formSteps.forEach(formStep => {
+    formSteps.forEach((formStep) => {
         formStep.classList.contains("active") &&
             formStep.classList.remove("active");
-    })
+    });
     formSteps[formStepsNum].classList.add("active");
 }
 
@@ -53,28 +64,27 @@ function updateProgressBar() {
         } else {
             progressStep.classList.remove("active");
         }
-    })
+    });
 
     const progressActive = document.querySelectorAll(".progress-step.active");
-    progress.style.width = ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + '%';
+    progress.style.width =
+        ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 }
 
-
-nextBtns.forEach(common_btn => {
+nextBtns.forEach((common_btn) => {
     common_btn.addEventListener("click", function () {
         formStepsNum++;
         updateFormSteps();
         updateProgressBar();
-        console.log("kk")
-    })
-})
+        console.log("kk");
+    });
+});
 
-
-prevBtns.forEach(common_btn => {
+prevBtns.forEach((common_btn) => {
     common_btn.addEventListener("click", function () {
         formStepsNum--;
         updateFormSteps();
         updateProgressBar();
-        console.log("kk")
-    })
-})
+        console.log("kk");
+    });
+});
